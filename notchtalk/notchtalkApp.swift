aa48c7daab140e071d91268f792481f6790682c8
Alex <alex@gogl.io>
2026-02-02 18:24:30 +0100

    Initial Commit

---
+++ notchtalk/notchtalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  notchtalkApp.swift
+//  notchtalk
+//
+//  Created by Alex Gogl on 02.02.26.
+//
+
+import SwiftUI
+
+@main
+struct notchtalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
